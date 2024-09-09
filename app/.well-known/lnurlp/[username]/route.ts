8b99f4a95c6f1fa7ea0e0ed614aedbe7c157241a
pcwd <41215821+pcwd@users.noreply.github.com>
2024-09-09 14:22:53 +1000

    Update route.ts
    Changed the receive message.

--- app/.well-known/lnurlp/[username]/route.ts
+++ app/.well-known/lnurlp/[username]/route.ts
@@ -16,6 +16,6 @@ export async function GET(
     callback: `${getBaseUrl()}/.well-known/lnurlp/${params.username}/callback`,
     minSendable: 1000,
     maxSendable: 10000000000,
-    metadata: `[["text/identifier","${params.username}@${domain}"],["text/plain","Sats for Alby Jim user ${params.username}"]]`,
+    metadata: `[["text/identifier","${params.username}@${domain}"],["text/plain","Sats received."]]`,
   });
 }
